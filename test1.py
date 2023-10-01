ff029ca1821ebe15c7b403b0ed38d76c66bb002e
DennisF <qpsa731@gmail.com>
2023-10-01 19:45:10 +0800

    init commit

---
+++ test1.py
@@ -0,0 +1,23 @@
+print('AAA')
+
+# comment1
+
+'''
+comment2
+'''
+
+"""
+comment3
+"""
+
+print(type('AAA'))
+print(type(123))
+print(type(123.0))
+
+a = 'Hi'
+print(a * 10)
+
+b = 'ABCD'
+print('C' in b)
+
+
